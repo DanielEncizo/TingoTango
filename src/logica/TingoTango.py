95ac760193ab02a97b423d722406a61a7fbce7b3
DanielEncizo <DanielEncizo>
2022-04-19 19:20:11 -0500

    Clase TingoTango refactorizada V4.1

--- src/logica/TingoTango.py
+++ src/logica/TingoTango.py
@@ -1,11 +1,14 @@
 class TingoTango:
-    def textoTingoTango(self,numero):
-        if (numero % 3 == 0) and (numero % 5 == 0):
-            return "TingoTango"
-        if(numero % 3 == 0):
-            return "Tingo"
+    def textoTingoTango(self, numero):
+        s = ""
+        if (numero % 3 == 0):
+            s += "Tingo"
         if (numero % 5 == 0):
-            return "Tango"
-        return str(numero)
+            s += "Tango"
+
+        if (len(s)):
+            return s
+        else:
+            return str(numero)
 
 
