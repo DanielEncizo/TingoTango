b26b843043e0cd6fce48224e11d71890083a69a4
DanielEncizo <DanielEncizo>
2022-04-19 19:13:57 -0500

    Múltiplo de 15 implementado V3.0

--- src/logica/TingoTango.py
+++ src/logica/TingoTango.py
@@ -1,6 +1,10 @@
 class TingoTango:
     def textoTingoTango(self,numero):
+        if (numero % 3 == 0) and (numero % 5 == 0):
+            return "TingoTango"
         if(numero % 3 == 0):
             return "Tingo"
         if (numero % 5 == 0):
             return "Tango"
+
+
